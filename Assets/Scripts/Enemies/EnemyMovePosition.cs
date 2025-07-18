70f9099cd9de61c3360f9f048739ac1e1b267458
Ashley N <ewannicholson2004@gmail.com>
2025-07-18 17:37:17 +0100

    Level 1
    Added all the stuff for level 1
    Made main menu dramatic

--- Assets/Scripts/Enemies/EnemyMovePosition.cs
+++ Assets/Scripts/Enemies/EnemyMovePosition.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class EnemyMovePosition : MonoBehaviour
 {
     public bool StandardMovement = true;
     public bool OffensiveMovement = true;
 
-    // Checks if player is currently overlapping this move position
+    [Tooltip("Objects on these layers will block this move position.")]
+    public LayerMask blockingLayers;
+
+    private EnemyAI parentAI;
+    private BoxCollider boxCollider;
+    private readonly HashSet<Collider> blockingObjects = new();
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+
+        parentAI = GetComponentInParent<EnemyAI>();
+        if (parentAI == null)
+        {
+            Debug.LogError($"EnemyMovePosition '{name}' has no EnemyAI on parent.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsBlockingLayer(other.gameObject.layer))
+        {
+            blockingObjects.Add(other);
+            UpdateRegistration();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (blockingObjects.Remove(other))
+        {
+            UpdateRegistration();
+        }
+    }
+
+    private bool IsBlockingLayer(int layer)
+    {
+        return (blockingLayers.value & (1 << layer)) != 0;
+    }
+
+    private void UpdateRegistration()
+    {
+        if (parentAI == null) return;
+
+        if (blockingObjects.Count > 0)
+        {
+            parentAI.movePositions.Remove(this);
+        }
+        else
+        {
+            if (!parentAI.movePositions.Contains(this))
+                parentAI.movePositions.Add(this);
+        }
+    }
     public bool IsPlayerOnPosition()
     {
-        // Assuming the MoveButton has a trigger collider attached for detection
-        Collider[] hits = Physics.OverlapBox(transform.position, Vector3.one * 0.4f, Quaternion.identity, LayerMask.GetMask("Player"));
+        Collider[] hits = Physics.OverlapBox(
+            transform.position,
+            Vector3.one * 0.4f,
+            Quaternion.identity,
+            LayerMask.GetMask("Player")
+        );
+
         return hits.Length > 0;
     }
 }
+
 
