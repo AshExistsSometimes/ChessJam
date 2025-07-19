b1637c34c71f8614103f362ef07b24ff05a99c31
Ashley N <ewannicholson2004@gmail.com>
2025-07-19 19:24:47 +0100

    Selection Indicators
    For movement buttons

--- Assets/Scripts/Player/MoveButton.cs
+++ Assets/Scripts/Player/MoveButton.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine;
+
 public class MoveButton : MonoBehaviour
 {
     public Collider overlapDetection;
@@ -9,10 +11,16 @@ public class MoveButton : MonoBehaviour
     public Collider inputDetectCollider;
 
     [SerializeField] private LayerMask obstacleMask;
+
+    [Header("Colors")]
     [SerializeField] private Color safeColor = Color.white;
     [SerializeField] private Color dangerColor = Color.red;
+    [SerializeField] private Color highlightSafeColor = new Color(1f, 1f, 1f, 1f); // Brighter white
+    [SerializeField] private Color highlightDangerColor = new Color(1f, 0.5f, 0.5f, 1f); // Brighter red
 
     private SpriteRenderer spriteRenderer;
+    private bool isDangerous;
+    private bool isHighlighted;
 
     private void Awake()
     {
@@ -49,9 +57,8 @@ public class MoveButton : MonoBehaviour
             ~LayerMask.GetMask("IgnoreRaycast")
         );
 
-        bool isDangerous = false;
-
         // Check for enemy offensive positions
+        isDangerous = false;
         Collider[] hits = Physics.OverlapBox(
             overlapDetection.bounds.center,
             overlapDetection.bounds.extents,
@@ -68,7 +75,10 @@ public class MoveButton : MonoBehaviour
             }
         }
 
-        spriteRenderer.color = isDangerous ? dangerColor : safeColor;
+        if (!isHighlighted)
+        {
+            spriteRenderer.color = isDangerous ? dangerColor : safeColor;
+        }
 
         SetActive(!isBlocked && hasGround);
     }
@@ -79,6 +89,22 @@ public class MoveButton : MonoBehaviour
         inputDetectCollider.enabled = state;
     }
 
+    private void OnMouseEnter()
+    {
+        if (!spriteRenderer.enabled) return;
+
+        isHighlighted = true;
+        spriteRenderer.color = isDangerous ? highlightDangerColor : highlightSafeColor;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!spriteRenderer.enabled) return;
+
+        isHighlighted = false;
+        spriteRenderer.color = isDangerous ? dangerColor : safeColor;
+    }
+
     private void OnMouseDown()
     {
         if (!GameManager.Instance.PlayersTurn) return;
@@ -86,5 +112,9 @@ public class MoveButton : MonoBehaviour
         PlayerMovement player = FindObjectOfType<PlayerMovement>();
         if (player != null)
             player.MoveTo(transform.position);
+
+        isHighlighted = false;
     }
+
 }
+
